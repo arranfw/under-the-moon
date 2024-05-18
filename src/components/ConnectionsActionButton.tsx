88454ba1c687c34cb04e4714940fbd1af9e256c7
Arran Woodruff <arranwoodruff@gmail.com>
2024-05-18 11:15:37 -0600

    responsive action buttons

--- src/components/ConnectionsActionButton.tsx
+++ src/components/ConnectionsActionButton.tsx
@@ -15,7 +15,7 @@ export const ConnectionsActionButton: React.FC<ConnectionsActionButton> = ({
     <button
       onClick={onClick}
       disabled={disabled}
-      className={cn("border rounded-full p-4 py-3", {
+      className={cn("border rounded-full p-4 py-3 md:text-lg text-base", {
         "border-black dark:border-white": !disabled,
         "text-black dark:text-white": !disabled,
         "border-gray-500": disabled,
