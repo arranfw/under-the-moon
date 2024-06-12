56fcd126f2e184f14019c7b1d6d92c4fedb5de46
Arran Woodruff <arranwoodruff@gmail.com>
2024-06-12 15:21:55 -0600

    avatar fallback up to 3 initials

--- src/util/index.ts
+++ src/util/index.ts
@@ -5,7 +5,7 @@ export const nameToFallbackText = (name: string | null) => {
   return (
     name
       ?.split(" ")
-      .slice(0, 2)
+      .slice(0, 3)
       .map((s) => s.charAt(0))
       .join("") || ""
   );
